d721c6d29d3d58b486bd682ebc66111ce458049a
Rudra <vbot34615@gmail.com>
2025-02-21 15:21:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,11 +7,11 @@ from pyrogram import filters
 load_dotenv()
 
 # Get this value from my.telegram.org/apps
-API_ID = int(getenv("API_ID", "28954982"))
+API_ID = int(getenv("API_ID", ""))
 API_HASH = getenv("API_HASH", "‎")
 
 # Get your token from @BotFather on Telegram.
-BOT_TOKEN = getenv("BOT_TOKEN", "7882219337:AAF29iOKJf3HF7Z_gPT6VuAfYq3uXOJrbM4")
+BOT_TOKEN = getenv("BOT_TOKEN", "")
 
 # Get your mongo url from cloud.mongodb.com
 MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://Raghav23:Raghav23@cluster0.6nrx6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
