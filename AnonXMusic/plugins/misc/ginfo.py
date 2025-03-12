e3d0231b645c1985ae03517c29692d5a4e3f9b42
Rudra <vbot34615@gmail.com>
2025-03-12 23:56:50 +0530

    Update ginfo.py

--- AnonXMusic/plugins/misc/ginfo.py
+++ AnonXMusic/plugins/misc/ginfo.py
@@ -116,6 +116,6 @@ async def user_info(client, message: Message):
     info_text += f"User ID: <code>{user_id}</code>\n"
     info_text += f"Language Code: {language_code}\n"
     info_text += f"Status: {status}\n\n"
-    info_text += f"🗂 Movie Bot @LB_Movies_Bot\n"
+    info_text += f"👨‍💻Developer @Information_It1\n"
 
     await message.reply_text(info_text)
